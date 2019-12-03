3e6eaef1791c999e997c58cbdca792ecbd2c5e43
John Doe <johndoe@example.com>
2019-12-03 15:06:56 -0800

    solver implemented in practiceSolver

--- solver.py
+++ solver.py
@@ -102,8 +102,14 @@ def solve(list_of_locations, list_of_homes, starting_car_location, adjacency_mat
     #                 openSet.add(neighbor)
     #
     # # failed
-    practiceSolver.tspRepeats(adjacency_matrix)
-    return trivial_output_solver(list_of_locations, list_of_homes, starting_car_location, adjacency_matrix)
+
+    homes = []
+    for i in list_of_homes:
+        homes.append(int(i))
+    print(practiceSolver.tspRepeats(adjacency_matrix, 0))
+    return [practiceSolver.tspRepeats(adjacency_matrix, 0)], {0: homes}
+
+    #return trivial_output_solver(list_of_locations, list_of_homes, starting_car_location, adjacency_matrix)
 
 
 
