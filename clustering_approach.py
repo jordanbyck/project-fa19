a1d63e75c6dc063c8183de7028757b0dc6217b41
Nik Yerasi <nyerasi@yahoo.com>
2019-12-05 23:32:12 -0800

    added vis func to clustering_approach

--- clustering_approach.py
+++ clustering_approach.py
@@ -13,7 +13,7 @@ def find_community_mappings(list_of_homes, adjacency_matrix):
     print("home clustering coeffs:", home_coeffs)
 
     # approach using python-louvain (community)
-    partition = community.best_partition(G)
+    partition = community.best_partition(G, weight='weight')
     all_communities = set(partition.values())
     community_mappings = {}
     for comm_label in all_communities:
@@ -37,6 +37,20 @@ def find_dropoff_locations(list_of_homes, adjacency_matrix, start, community_map
             dropoffs[label] = current_location
     return dropoffs
 
-def visualize_communities_and_dropoffs(G, start):
+def visualize_communities_and_dropoffs(list_of_locations, list_of_homes, starting_car_location, adjacency_matrix):
     # draw each community in a different color with its dropoff node
-    print("wip")
+    G = student_utils.adjacency_matrix_to_graph(adjacency_matrix)[0]
+    comm_mappings = find_community_mappings(list_of_homes, adjacency_matrix)
+    comm_dropoffs = find_dropoff_locations(list_of_homes, adjacency_matrix, starting_car_location, comm_mappings)
+    color_map = [0 for n in range(len(list_of_locations))]
+    current_color = 0
+    for comm_label in comm_mappings:
+        locs = comm_mappings[comm_label]
+        for loc in locs:
+            color_map[loc] = current_color
+        current_color += 1.0/len(comm_mappings.keys())
+    color_map[int(starting_car_location)] = 1
+    labels = nx.get_edge_attributes(G, 'weight')
+    nx.draw(G, node_color=color_map, with_labels=True, font_weight='bold')
+    nx.draw_networkx_edge_labels(G, pos=nx.spring_layout(G), edge_labels=labels)
+    plt.show()
